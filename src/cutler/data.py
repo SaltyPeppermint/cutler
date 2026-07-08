f3545a4724cc28a8893c71f9cf2d73b61189ed03
dexterlb <dexterlb@qtrp.org>
2026-07-08 17:06:46 +0300

    define filter base

--- src/cutler/data.py
+++ src/cutler/data.py
@@ -1,15 +1,20 @@
 import ffmpeg
 from dataclasses import dataclass, field
-
-@dataclass
-class Chain:
-    inputs: list[str] = field(default_factory = list)   # chain inputs are names of other chains
-    filters: list() = field(default_factory = list)
+from typing import Protocol, runtime_checkable
 
 @dataclass
 class Source:   # TODO: naming hard
     strm: ffmpeg.Stream
     actual_duration: float
+
+@runtime_checkable
+class Filter(Protocol):
+    def filterify(self, *srcs: Source) -> Source: ...
+
+@dataclass
+class Chain:
+    inputs: list[str] = field(default_factory = list)   # chain inputs are names of other chains
+    filters: list[Filter] = field(default_factory = list)
 
 @dataclass
 class Job:
