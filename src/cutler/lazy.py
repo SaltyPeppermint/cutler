dd2df1781020c93d6ed3c7a120add4afc24729ed
dexterlb <dexterlb@qtrp.org>
2026-07-09 21:21:34 +0300

    async-safe lazy

--- src/cutler/lazy.py
+++ src/cutler/lazy.py
@@ -1,20 +1,26 @@
+import asyncio
 import inspect
-from typing import Callable
 
 class Lazy[T]:
     def __init__(self, f):
         self._f = f
         self._computed = False
         self._val = None
+        self._lock = asyncio.Lock()
 
     async def get(self) -> T:
         if self._computed:
             return self._val
 
-        val = self._f()
-        if inspect.isawaitable(val):
-            val = await val
+        async with self._lock:
+            # another get() may have computed the value while we waited for the lock
+            if self._computed:
+                return self._val
 
-        self._val = val
-        self._computed = True
-        return self._val
+            val = self._f()
+            if inspect.isawaitable(val):
+                val = await val
+
+            self._val = val
+            self._computed = True
+            return self._val
